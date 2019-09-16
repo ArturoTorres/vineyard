67ba76579656256000c3ed7998f8db137f5c38f7
ArturoTorres <arturo.torres@list.lu>
2019-09-16 17:34:02 +0200

    updated budburst models by Daniel et al (2014)

--- R/BudBurst.R
+++ R/BudBurst.R
@@ -1,15 +1,18 @@
 #' Compute the degree-days by the single triangle algorithm
 #'
-#' @param x xts object containing the input data.
-#' @param t.min daily minimum air temperature.
-#' @param t.max daily maximum air temperature.
-#' @param t.zero threshold temperature for vine growth.
+#' Implementation to compute the degree-days by the single triangle algorithm by Nendel (2010).
 #'
-#' @return a vector with the degree-days for vine growth.
+#' @param t.min daily minimum air temperature vector in Celsius degrees.
+#' @param t.max daily maximum air temperature vector in Celsius degrees.
+#' @param t.zero numeric, threshold temperature (in Celsius degrees) for vine growth.
+#'
+#' @return a vector with the degree-days (in Celsius degrees) for vine growth.
 #'
 #' @importFrom xts xts
+#'
+#' @references Nendel, Class (2010). Grapevine bud break prediction for cool winter climates. Int. J. Biometeorol., 54, 231–241.
 
-DD.single.triang <- function(t.zero, t.min, t.mean, t.max){
+DD.single.triangle <- function(t.zero, t.min, t.mean, t.max){
   if(t.zero >= t.max){
     dd <- 0
   } else if((t.zero > t.min) && (t.zero < t.max)){
@@ -21,3 +24,85 @@ DD.single.triang <- function(t.zero, t.min, t.mean, t.max){
 }
 
 
+#' Compute the degree-days by the single temperature threshold
+#'
+#' Implementation to compute the degree-days by the single temperature threshold by Molitor et al., (2014).
+#'
+#' @param t.mean daily mean air temperature vector in Celsius degrees.
+#' @param a numeric, threshold temperature (in Celsius degrees) for vine growth.
+#'
+#' @return a vector with the degree-days (in Celsius degrees) for vine growth.
+#'
+#' @importFrom xts xts
+#'
+#' @references Daniel Molitor, Jürgen Junk, Danièle Evers, Lucien Hoffmann, and Marco Beyer.
+#' A high-resolution cumulative degree day-based model to simulate phenological development of grapevine.
+#' Am. J. Enol. Vitic., (65:1):72–80, 2014.
+
+DD.single.threshold <- function(t.mean, a){
+  if(t.mean < a){
+    dd <- 0
+  } else if(a < t.mean){
+    dd <- (t.mean - a)
+  }
+  return(dd)
+}
+
+
+#' Compute the degree-days by the lower and upper temperature thresholds
+#'
+#' Implementation to compute the degree-days by the lower and upper temperature thresholds by Molitor et al., (2014).
+#'
+#' @param t.mean daily mean air temperature vector in Celsius degrees.
+#' @param a numeric, lower threshold temperature (in Celsius degrees) for vine growth.
+#' @param b numeric, upper threshold temperature (in Celsius degrees) for vine growth.
+#'
+#' @return a vector with the degree-days (in Celsius degrees) for vine growth.
+#'
+#' @importFrom xts xts
+#'
+#' @references Daniel Molitor, Jürgen Junk, Danièle Evers, Lucien Hoffmann, and Marco Beyer.
+#' A high-resolution cumulative degree day-based model to simulate phenological development of grapevine.
+#' Am. J. Enol. Vitic., (65:1):72–80, 2014.
+
+DD.double.threshold <- function(t.mean, a, b){
+  if(t.mean < a){
+    dd <- 0
+  } else if((a < t.mean) && (t.mean < b)){
+    dd <- (t.mean - a)
+  } else if(b < t.mean){
+    dd <- (b - a)
+  }
+  return(dd)
+}
+
+
+#' Compute the degree-days by a heat threshold temperature
+#'
+#' Implementation to compute the degree-days by a heat threshold temperature by Molitor et al., (2014).
+#'
+#' @param t.mean daily mean air temperature vector in Celsius degrees.
+#' @param a numeric, lower threshold temperature (in Celsius degrees) for vine growth.
+#' @param b numeric, upper threshold temperature (in Celsius degrees) for vine growth.
+#' @param c numeric, heat threshold temperature (in Celsius degrees) for vine growth.
+#'
+#' @return a vector with the degree-days (in Celsius degrees) for vine growth.
+#'
+#' @importFrom xts xts
+#'
+#' @references Daniel Molitor, Jürgen Junk, Danièle Evers, Lucien Hoffmann, and Marco Beyer.
+#' A high-resolution cumulative degree day-based model to simulate phenological development of grapevine.
+#' Am. J. Enol. Vitic., (65:1):72–80, 2014.
+
+DD.heat.threshold <- function(t.mean, a, b, c){
+  if((t.mean < a) || (c + (b - a) < t.mean)){
+    dd <- 0
+  } else if((a < t.mean) && (t.mean < b)){
+    dd <- (t.mean - a)
+  } else if(b < t.mean){
+    dd <- (b - a)
+  } else if(c < t.mean) {
+    dd <- (b - a) - (t.mean - c)
+  }
+  return(dd)
+}
---
+++ R/Datasets.R
@@ -0,0 +1,11 @@
+#' Sample time series of meteorological data for Remich station.
+#'
+#' A sample dataset containing time series of meteorological data for Remich station located in the
+#' Grand-Duchy of Luxembourg obtained by the Institut Viti-vinicole in Remich.
+#' The data covers the period from 1970 to 2017 at daily time interval.
+#'
+#' @name Remich
+#' @docType data
+#' @references \url{https://agriculture.public.lu/de/weinbau-oenologie.html}
+#' @keywords data
+NULL
