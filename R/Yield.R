001e0bc80981964e0c271dec3d26ff21840a2b2b
ArturoTorres <arturo.torres@list.lu>
2022-02-02 09:29:21 +0100

    updated yield models for Riesling and Rivaner

--- R/Yield.R
+++ R/Yield.R
@@ -16,12 +16,12 @@
 lm.yield.applied.riesling <- function(stat.rain0.ries, stat.tmin1.ries, stat.tmin01.ries,
                                       stat.rain1.ries, stat.tmin02.ries, model){
 
-  stats <- cbind.data.frame(year   = stat.tmin1.ries[,1],
-                            a = stat.rain0.ries[,2],
-                            b = stat.tmin1.ries[,2],
-                            c = stat.tmin01.ries[,2],
-                            d = stat.rain1.ries[,2],
-                            e = stat.tmin02.ries[,2]
+  stats <- cbind.data.frame(year   = stat.rain0.ries[2:nrow(stat.rain0.ries),1],
+                            a = stat.rain0.ries[2:nrow(stat.rain0.ries),2],
+                            b = stat.tmin1.ries[1:(nrow(stat.tmin1.ries)-1),2],
+                            c = stat.tmin01.ries[2:nrow(stat.tmin01.ries),2],
+                            d = stat.rain1.ries[1:(nrow(stat.rain1.ries)-1),2],
+                            e = stat.tmin02.ries[2:nrow(stat.tmin02.ries),2]
                             )
 
   My.yield.riesling <- function(vars, model){
@@ -57,7 +57,7 @@ lm.yield.applied.riesling <- function(stat.rain0.ries, stat.tmin1.ries, stat.tmi
 
 lm.yield.applied.rivaner <- function(stat.tmin1, stat.tmean1, stat.tmax1, stat.tmean0, stat.rain01, stat.rain02, stat.tmin0, model){
 
-  stats <- cbind.data.frame(year   = stat.tmin1[,1],
+  stats <- cbind.data.frame(year   = stat.tmean0[,1],
                             a = stat.tmin1[,2],
                             b = stat.tmean1[,2],
                             c = stat.tmax1[,2],
